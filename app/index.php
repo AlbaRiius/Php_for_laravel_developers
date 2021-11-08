a7cca4a4ff46e15e1956c41b60092fbb52896ec7
AlbaRiius <arius@iesebre.com>
2021-11-08 19:13:28 +0100

    Versió 0.8. Petita refactorització per utilitzar els helpers amb composer autoload

--- app/index.php
+++ app/index.php
@@ -3,7 +3,6 @@
 use framework\App;
 use framework\Database\Database;
 
-require 'helpers.php';
 
 //$database = new Database(App::get('config')['database']);
 $tasks = App::get('database')->selectAll('tasks');
